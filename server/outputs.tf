8f80bb2bd2293d164afbe00b7533df1f75ed2be5
Jeremy Wood <jeremy.wood@7factor.io>
2021-12-30 14:43:48 -0500

    Fix module outputs.

--- server/outputs.tf
+++ server/outputs.tf
@@ -1,8 +1,8 @@
 output "transfer_server" {
   description = "The details of the Transfer server"
-  value = object({
+  value = {
     arn      = aws_transfer_server.transfer_server.arn
     id       = aws_transfer_server.transfer_server.id
     endpoint = aws_transfer_server.transfer_server.endpoint
-  })
+  }
 }
