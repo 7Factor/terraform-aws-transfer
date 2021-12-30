4cb0b3e252a8b7486c2fdd1ccfa4db94febfbb51
Jeremy Wood <jeremy.wood@7factor.io>
2021-12-30 09:45:24 -0500

    Nix "namespacing" of IAM resource names.

--- users/iam.tf
+++ users/iam.tf
@@ -47,26 +47,26 @@ data "aws_iam_policy_document" "s3_read_only" {
 
 resource "aws_iam_role" "s3_read_write_role" {
   count              = var.read_write_users ? 1 : 0
-  name               = "${var.service_name}_transfer-s3-read-write-role"
+  name               = "${var.service_name}-transfer-s3-read-write-role"
   assume_role_policy = data.aws_iam_policy_document.transfer_assume_role.json
 }
 
 resource "aws_iam_role_policy" "transfer_s3_read_write_policy" {
   count  = var.read_write_users ? 1 : 0
-  name   = "${var.service_name}_transfer-s3-read-write-policy"
+  name   = "${var.service_name}-transfer-s3-read-write-policy"
   policy = data.aws_iam_policy_document.s3_read_write.json
   role   = aws_iam_role.s3_read_write_role.name
 }
 
 resource "aws_iam_role" "s3_read_only_role" {
   count              = var.read_only_users ? 1 : 0
-  name               = "${var.service_name}_transfer-s3-read-only-role"
+  name               = "${var.service_name}-transfer-s3-read-only-role"
   assume_role_policy = data.aws_iam_policy_document.transfer_assume_role.json
 }
 
 resource "aws_iam_role_policy" "transfer_s3_read_only_policy" {
   count  = var.read_only_users ? 1 : 0
-  name   = "${var.service_name}_transfer-s3-read-only-policy"
+  name   = "${var.service_name}-transfer-s3-read-only-policy"
   policy = data.aws_iam_policy_document.s3_read_only.json
   role   = aws_iam_role.s3_read_only_role.name
 }
