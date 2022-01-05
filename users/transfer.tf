2eb4ae67e4ed3b49f8955870903f8f66bee1a6a9
Jeremy Wood <jeremy.wood@7factor.io>
2022-01-05 13:13:04 -0500

    Fix copy/paste mistake in users/transfer.tf.
    Adding read/write users checked values in the list of read only users which is definitely not what it should be doing.

--- users/transfer.tf
+++ users/transfer.tf
@@ -18,7 +18,7 @@ resource "aws_transfer_ssh_key" "transfer_server_read_write_user_ssh_key" {
   count     = length(var.read_write_users)
   server_id = var.transfer_server_id
   user_name = element(var.read_write_users.*.user_name, count.index)
-  body      = element(var.read_only_users.*.ssh_public_key, count.index)
+  body      = element(var.read_write_users.*.ssh_public_key, count.index)
 }
 
 resource "aws_transfer_ssh_key" "transfer_server_read_only_user_ssh_key" {
