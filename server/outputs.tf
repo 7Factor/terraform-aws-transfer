b2d1194f8d457c300d38afeefc63a9d855e17a6b
Brian Voskerijian <brian.voskerijian@7factor.io>
2022-01-11 16:58:37 -0500

    Output VPC endpoint details

--- server/outputs.tf
+++ server/outputs.tf
@@ -1,8 +1,16 @@
 output "transfer_server" {
   description = "The details of the Transfer server"
-  value = {
+  value       = {
     arn      = aws_transfer_server.transfer_server.arn
     id       = aws_transfer_server.transfer_server.id
     endpoint = aws_transfer_server.transfer_server.endpoint
+  }
+}
+
+output "transfer_server_vpc_endpoint" {
+  description = "The details of the associated VPC endpoint"
+  value       = {
+    id = data.aws_vpc_endpoint.transfer_endpoint.id
+    dns_entry = data.aws_vpc_endpoint.transfer_endpoint.dns_entry
   }
 }
